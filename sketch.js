ef50a7a60b374e0ba5cd7d40635f084416d6061c
limkokyoong <57053488+limkokyoong@users.noreply.github.com>
2024-09-19 00:53:19 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+let audio;
+let N = 20;
+let x = 40;
+let y = 40;
+
+function preload(){
+  song = loadSound('audiodemo.mp3');
+}
+
+function setup() {
+  let cnv = createCanvas(windowWidth, windowHeight);
+  // cnv.mousePressed(userStartAudio);
+  // userStartAudio();
+  background(0);
+  stroke(255);
+  
+  //creates a new instance of the p5.AudioIn object and assigns it to the mic variable.
+  // mic = new p5.AudioIn();
+  //starts the microphone input.
+  // mic.start();
+  
+  // Create a new Amplitude analyzer
+  amplitude = new p5.Amplitude();
+  
+  // Play the loaded song (optional: loop)
+  song.play();
+  
+}
+
+function draw() {
+  
+  // use the getLevel() method to read the amplitude (volume level) of the audio.
+  // micLevel = mic.getLevel();
+  let audioLevel = amplitude.getLevel();
+  // now you have assigned the micLevel value to a variable called micLevel so you can use this to modulate the properties of your pattern
+  let M = audioLevel*N;
+  
+  stroke(255);
+  strokeWeight(M);
+  
+  
+  if (y <= height - 40) {
+    line(x - 5, y - 5, x + 5, y + 5);
+    ellipse(x,y,M*10,M*10);
+    x += 25;
+  }
+  if (x > width - 40) {
+    x = 40;
+    y += 10;
+  }
+  if (y >= height - 40) {
+    x = 40;
+    y = 40;
+    background(0);
+  }
+  // console.log(micLevel);
+}
+
+// function mousePressed() {
+//   mic.start();
+// }
+
+function mousePressed() {
+  if (!song.isPlaying()) {
+    song.play();
+  } else {
+    song.pause();
+  }
+}
+
+
